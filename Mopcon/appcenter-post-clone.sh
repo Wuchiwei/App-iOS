7bfc8643a05b106f93c687bbcdbcb5e482180654
Luke <wuchiwei17@gmail.com>
2019-05-16 17:44:00 +0800

    Add post clone shell script for App Center.
    new file:   appcenter-post-clone.sh

---
+++ Mopcon/appcenter-post-clone.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+openssl des-cbc -d -k MOPCON201905161737 -in secrets.tar.enc -out secrets.tar
+tar xvf ./secrets.tar
+cd app_json
+mv Quiz.json ..
+mv BoothMission.json ..
+cd ..
+mv Quiz.json Mopcon/Mopcon/JSON
+mv BoothMission.json Mopcon/Mopcon/JSON
